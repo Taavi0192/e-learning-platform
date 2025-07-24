58ae21a1386617254307223608338367a5c557f1
Taavi <sabeehazam123@gmail.com>
2025-07-24 18:48:35 +0500

    .

--- server/routes/passwordReset.js
+++ server/routes/passwordReset.js
@@ -1,14 +1,14 @@
-import express from "express";
-import {
-  forgotPassword,
-  verifyOTP,
-  resetPassword,
-} from "../controllers/passwordResetController.js";
-
-const router = express.Router();
-
-router.post("/forgot-password", forgotPassword);
-router.post("/verify-otp", verifyOTP);
-router.post("/reset-password", resetPassword);
-
-export default router;
+// import express from "express";
+// import {
+//   forgotPassword,
+//   verifyOTP,
+//   resetPassword,
+// } from "../controllers/passwordResetController.js";
+//
+// const router = express.Router();
+//
+// router.post("/forgot-password", forgotPassword);
+// router.post("/verify-otp", verifyOTP);
+// router.post("/reset-password", resetPassword);
+//
+// export default router;
